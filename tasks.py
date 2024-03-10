253c726897f8b52bca785ec48a9c0651c22b55b7
Jacob Hightower <jah795@nau.edu>
2024-03-09 17:50:25 -0800

    Add HTTP and Excel dependencies, and implement filling sales form from Excel data

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from robocorp.tasks import task
 from robocorp import browser, log
 import json
+from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
 
 @task
 def robot_spare_bin_python():
@@ -11,7 +13,8 @@ def robot_spare_bin_python():
     open_the_intranet_website()
     with log.suppress_variables():
         log_in()
-    fill_and_submit_sales_form()
+    download_excel_file()
+    fill_form_with_excel_data()
     
 def load_config(config_path):
     try:
@@ -39,13 +42,28 @@ def log_in():
     page.fill("#password", password)
     page.click("button:text('Log in')")
 
-def fill_and_submit_sales_form():
-    """Fills in the sales data and clicks the 'Submit' button."""
+def fill_form_with_excel_data():
+    """Read data from excel and fill in the sales form."""
+    
+    excel = Files()
+    excel.open_workbook("SalesData.xlsx")
+    worksheet = excel.read_worksheet_as_table("data", header=True)
+    excel.close_workbook()
+
+    for row in worksheet:
+        fill_and_submit_sales_form(row)
+
+def fill_and_submit_sales_form(sales_rep):
+    """Fills in the sales data and click the 'Submit' button"""
     page = browser.page()
 
-    page.fill("#firstname", "John")
-    page.fill("#lastname", "Smith")
-    page.fill("#salesresult", "123")
-    page.select_option("#salestarget", "10000")
+    page.fill("#firstname", sales_rep["First Name"])
+    page.fill("#lastname", sales_rep["Last Name"])
+    page.select_option("#salestarget", str(sales_rep["Sales Target"]))
+    page.fill("#salesresult", str(sales_rep["Sales"]))
     page.click("text=Submit")
-    
+
+def download_excel_file():
+    """Downloads the Excel file from the intranet."""
+    http = HTTP()
+    http.download("https://robotsparebinindustries.com/intranet/SalesData.xlsx", overwrite=True)
